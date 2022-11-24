19f220c7833e88996f5df76f35189fff1e516f71
Leonard Palm <leonard4001@gmail.com>
2022-11-24 16:57:43 +0100

    Made StateEventWithContent a sealed interface instead of abstract class

--- compose-state-events/src/main/java/de/palm/composestateevents/StateEventWithContent.kt
+++ compose-state-events/src/main/java/de/palm/composestateevents/StateEventWithContent.kt
@@ -3,20 +3,20 @@ package de.palm.composestateevents
 /**
  *  This [StateEventWithContent] can have exactly 2 states like the [StateEvent] but the triggered state holds a value of type [T].
  */
-abstract class StateEventWithContent<T>
+sealed interface StateEventWithContent<T>
 
 /**
  * The event in its triggered state holding a value of [T]. See [triggered] to create an instance of this.
  * @param content A value that is needed on the event consumer side.
  */
-class StateEventWithContentTriggered<T>(val content: T) : StateEventWithContent<T>(){
+class StateEventWithContentTriggered<T>(val content: T) : StateEventWithContent<T>{
     override fun toString(): String = "triggered($content)"
 }
 
 /**
  * The event in its consumed state not holding any value. See [consumed] to create an instance of this.
  */
-class StateEventWithContentConsumed<T> : StateEventWithContent<T>(){
+class StateEventWithContentConsumed<T> : StateEventWithContent<T>{
     override fun toString(): String = "consumed"
 }
 
